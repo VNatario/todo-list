7d8f979842f6e5179ef3977e417b15c7e2478063
Vitor Natario <natariovitor@gmail.com>
2023-06-21 19:22:40 -0300

    add wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { CreateTask } from "./components/CreateTask";
 import { Header } from "./components/Header";
 
+import styles from "./App.module.css";
+
 export function App() {
   return (
     <>
       <Header />
-      <CreateTask />
+      <div className={styles.wrapper}>
+        <CreateTask />
+      </div>
     </>
   );
 }
